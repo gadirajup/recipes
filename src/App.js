78cb5b73f7f9221a729ef1571b0414eacd9855f1
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-30 16:11:28 -0700

    unathorized api request

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "d8c7d2c4";
+  const APP_KEY = "925872534c2dc547bad3f3fdd2a69c6b";
+  const PROXY = 'https://cors-anywhere.herokuapp.com/'
+
+  useEffect( () => {
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => { 
+    const response = await fetch(`${PROXY}https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`, {
+      method: 'post',
+      headers: {},
+      body: {}
+    })
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className = "search-form">
+        <input className="search-bar" type="text"></input>
+        <button className="search-button" type="submit">
+        Search
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
